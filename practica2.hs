819673fb312a4084f6a3cb78d035e6f5e865957b
aitsebdog <aldocmx15@gmail.com>
2024-11-12 20:44:24 -0600

    se quitaon las funciones auxiliares tambien quita un caso base en agregar elemento

--- practica2.hs
+++ practica2.hs
@@ -1,11 +1,3 @@
---------------- Funciones auxiliares ---------------
-maximo :: (Num a, Ord a) => a -> a -> a
-maximo x y = if x==y
-                   then x
-                 else if x>y
-                      then x
-                 else y
-
 --------------- Listas y recursión ---------------
 
 longitud :: [a] -> Int
@@ -17,16 +9,19 @@ sumaLista [] = 0
 sumaLista (x:xs) = x + sumaLista (xs)
 
 agregaElemento :: [a] -> a -> Bool -> [a]
-agregaElemento (x:xs) a True = [a] ++ (x:xs)
-agregaElemento [] a False = [a]
-agregaElemento (x:xs) a False = [x] ++ agregaElemento xs a False
+agregaElemento (x:xs) a True = a : (x:xs)
+agregaElemento (x:xs) a False = (x:xs) ++ [a] 
 
 maximoLista :: (Num a, Ord a) => [a] -> a
-maximoLista [x] = x
-maximoLista (x:xs) = maximo x (maximoLista xs)
+maximoLista [x] = x  
+maximoLista (x:xs) = if x > maximoLista xs  
+                    then x                   
+                    else maximoLista xs   
+                     
 
 indice :: [a] -> Int -> a
 indice (x:xs) 0 = x
+indice []_ = error "Indice invalido"
 indice (x:xs) n = if n >= longitud (x:xs) || n<0
                      then error "El indice dado no pertenece al rango"
                   else if n == 0 
@@ -44,4 +39,8 @@ conjunto [] = []
 conjunto (x:xs) = x : conjunto[y | y <- xs , y /= x ]
 
 numerosPares :: [Int] -> [Int]
-numerosPares (x:xs) = [n | n <- (x:xs), mod n 2 == 0]
+numerosPares [] = []
+numerosPares (x:xs) = if x `div` 2 * 2 == x  
+                       then x : numerosPares xs  
+                      else numerosPares xs
+     
